5840f8b2175238a9ad2da563c92b1c569348341c
mj-rahmadwijaya <rahmad.wijaya@magic-jungle.co.id>
2025-05-06 13:52:11 +0700

    rxdb

--- index.js
+++ index.js
@@ -3,6 +3,49 @@ const { Notification } = require('electron')
 const { app, BrowserWindow, ipcMain, dialog, Menu, nativeTheme, globalShortcut } = require('electron/main')
 const path = require('node:path')
 
+const { addRxPlugin } = require('rxdb');
+const { createRxDatabase } = require('rxdb');
+const { wrappedValidateAjvStorage } = require('rxdb/plugins/validate-ajv');
+const { getRxStorageMemory } = require('rxdb/plugins/storage-memory');
+const { RxDBDevModePlugin } = require('rxdb/plugins/dev-mode');
+
+addRxPlugin(RxDBDevModePlugin);
+
+const initDatabase = async () => {
+    const myDatabase = await createRxDatabase({
+        name: 'db-electron',
+        storage: wrappedValidateAjvStorage({
+            storage: getRxStorageMemory()
+        })
+    });
+
+    await myDatabase.addCollections({
+        todos: {
+            schema: {
+                version: 0,
+                primaryKey: 'id',
+                type: 'object',
+                properties: {
+                    id: {
+                        type: 'string',
+                        maxLength: 100 // <- the primary key must have maxLength
+                    },
+                    name: {
+                        type: 'string'
+                    },
+                    done: {
+                        type: 'boolean'
+                    }
+                },
+                required: ['id', 'name', 'done']
+            }
+        }
+    });
+
+    return myDatabase
+}
+
+
 const createWindow = () => {
     const mainWindow = new BrowserWindow({
         width: 800,
@@ -42,13 +85,13 @@ const createWindow = () => {
             ]
         }
     ])
-      
+
     ///menu bar
     // Menu.setApplicationMenu(menu)
 
     // mainWindow.loadFile('index.html')
     mainWindow.loadURL('http://localhost:3001')
-    
+
 }
 
 async function handleFileOpen() {
@@ -72,28 +115,60 @@ async function fetchProductCategoryList() {
     const data = await response.json();
     return data
 }
- 
+
 async function fetchProductByCategory(category) {
     const response = await fetch(`https://dummyjson.com/products/category/${category}`)
     const data = await response.json();
     return data
 }
+async function cretedDbRxDB() {
+    global.db = await initDatabase();
+}
+
+async function insertToDB(item) {
+    await db.todos.insert(item);
+}
 
 app.whenReady().then(() => {
-    
+    createWindow()
+    cretedDbRxDB()
     // new Notification({ title: "Show notification", body: "Check logs! bla bla bla" }).show();
 
     globalShortcut.register('Alt+CommandOrControl+I', () => {
         console.log('Electron loves global shortcuts!')
-      })
+    })
     ipcMain.on('counter-value', (_event, value) => {
         console.log(value) // will print value to Node console
     })
     ipcMain.handle('dialog:openFile', handleFileOpen)
-    createWindow()
+
     ipcMain.handle('fetch-product-category-list', fetchProductCategoryList)
     ipcMain.handle('fetch-product-by-category', async (event, category) => {
         return await fetchProductByCategory(category);
+    })
+
+    ipcMain.handle('get-item-db', async () => {
+        const x = await db.todos.find().exec();
+        return JSON.stringify(x);
+    })
+    ipcMain.handle('insert-to-db', async (event, item) => {
+        await insertToDB(item);
+    })
+    ipcMain.handle('update-item-db', async (event, item) => {
+        const todo = await db.todos.findOne(item.id).exec()
+        await todo.patch({
+            name: item.name
+        })
+    })
+    ipcMain.handle('delete-item-db', async (event, id) => {
+        const todo = await db.todos.findOne(id).exec()
+        await todo.remove();
+    })
+    ipcMain.handle('subscribe-item-db', async () => {
+        const x = await db.todos.find().$.subscribe(results => {
+            console.log('----', results.length);
+        })
+        return '';
     })
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
--- preload.js
+++ preload.js
@@ -7,5 +7,10 @@ contextBridge.exposeInMainWorld('electronAPI', {
   counterValue: (value) => ipcRenderer.send('counter-value', value),
   toggleMode: () => ipcRenderer.invoke('dark-mode:toggle'),
   fetchProductCategoryList: () => ipcRenderer.invoke('fetch-product-category-list'),
-  fetchProductByCategory: (category) => ipcRenderer.invoke('fetch-product-by-category', category), 
+  fetchProductByCategory: (category) => ipcRenderer.invoke('fetch-product-by-category', category),
+  insertToDB: (item) => ipcRenderer.invoke('insert-to-db', item),
+  getItemDB: () => ipcRenderer.invoke('get-item-db'),
+  subscribeItemDB: () => ipcRenderer.invoke('subscribe-item-db'),
+  updateItemDB: (item) => ipcRenderer.invoke('update-item-db', item),
+  deleteItemDB: (id) => ipcRenderer.invoke('delete-item-db', id),
 })
